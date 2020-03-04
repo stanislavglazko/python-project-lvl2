813cb083daf1daf8f9752adc8b5f802172a6f775
stanislavglazko <stanislavglazko@gmail.com>
2020-03-04 22:39:12 +0300

    generate_diff_ver3

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,19 +4,16 @@ import json
 def generate_diff(source1, source2):
     source1 = json.load(open(source1))
     source2 = json.load(open(source2))
-    common = source1.keys() & source2.keys()
-    only_before = source1.keys() - source2.keys()
-    only_after = source2.keys() - source1.keys()
     result = '{' + '\n'
-    for i in common:
+    for i in source1.keys() & source2.keys():
         if source1[i] == source2[i]:
             result = result + '   ' + i + ': ' + str(source1[i]) + '\n'
         else:
             result = result + ' + ' + i + ': ' + str(source2[i]) + '\n'
             result = result + ' - ' + i + ': ' + str(source1[i]) + '\n'
-    for i in only_before:
+    for i in source1.keys() - source2.keys():
         result = result + ' - ' + i + ': ' + str(source1[i]) + '\n'
-    for i in only_after:
+    for i in source2.keys() - source1.keys():
         result = result + ' + ' + i + ': ' + str(source2[i]) + '\n'
     result = result + '}'
     return result
