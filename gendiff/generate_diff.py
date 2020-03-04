2b514206bebfcd97d8f438f16e1316f34d639bb6
stanislavglazko <stanislavglazko@gmail.com>
2020-03-04 22:33:27 +0300

    new_generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,26 +2,21 @@ import json
 
 
 def generate_diff(source1, source2):
-    before = json.load(open(source1))
-    after = json.load(open(source2))
-    before_keys = before.keys()
-    after_keys = after.keys()
-    common = before_keys & after_keys
-    only_before = before_keys - after_keys
-    only_after = after_keys - before_keys
-    result = ['{', ]
+    source1 = json.load(open(source1))
+    source2 = json.load(open(source2))
+    common = source1.keys() & source2.keys()
+    only_before = source1.keys() - source2.keys()
+    only_after = source2.keys() - source1.keys()
+    result = '{' + '\n'
     for i in common:
-        if before[i] == after[i]:
-            result.append('   ' + i + ': ' + str(before[i]))
+        if source1[i] == source2[i]:
+            result = result + '   ' + i + ': ' + str(source1[i]) + '\n'
         else:
-            result.append('+  ' + i + ': ' + str(after[i]))
-            result.append('-  ' + i + ': ' + str(before[i]))
+            result = result + ' + ' + i + ': ' + str(source2[i]) + '\n'
+            result = result + ' - ' + i + ': ' + str(source1[i]) + '\n'
     for i in only_before:
-        result.append('-  ' + i + ': ' + str(before[i]))
+        result = result + ' - ' + i + ': ' + str(source1[i]) + '\n'
     for i in only_after:
-        result.append('+  ' + i + ' ' + str(after[i]))
-    result.append('}')
-    answer = ''
-    for i in result:
-        answer = answer + i + '\n'
-    return answer
+        result = result + ' + ' + i + ': ' + str(source2[i]) + '\n'
+    result = result + '}'
+    return result
