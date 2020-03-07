da1390df3dc56e0a81ac8777c66d334ada96d752
stanislavglazko <stanislavglazko@gmail.com>
2020-03-07 14:39:55 +0300

    new_Makefile

--- Makefile
+++ Makefile
@@ -7,12 +7,12 @@ lint:
 selfcheck:
 	poetry check
 
-check: selfcheck test lint
+check: selfcheck lint
 
 build: check
 	@poetry build
 
 run_test:
-	@poetry run pytest --cov=gendiff tests/ --cov-report xml
+	@poetry run pytest --cov tests/ --cov-report xml
 
 .PHONY: install test lint selfcheck check build
--- tests/tests_answers.py
+++ tests/tests_answers.py
@@ -1,20 +1,21 @@
 import json
+import pytest
 from gendiff.generate_diff import generate_diff
 
 
 def test_answer():
     fin = open('./tests/fixtures/answer.txt', 'r')
     answer = fin.read()
-    assert generate_diff('before.json', 'after.json') == answer
+    assert generate_diff('./tests/fixtures/before.json','./tests/fixtures/after.json') == answer
 
 
 def test_answer2():
     fin = open('./tests/fixtures/answer2.txt', 'r')
     answer = fin.read()
-    assert generate_diff('before2.json', 'after2.json') == answer
+    assert generate_diff('./tests/fixtures/before2.json', './tests/fixtures/after2.json') == answer
 
 
 def test_answer3():
     fin = open('./tests/fixtures/answer3.txt', 'r')
     answer = fin.read()
-    assert generate_diff('before3.json', 'after3.json') == answer
+    assert generate_diff('./tests/fixtures/before3.json', './tests/fixtures/after3.json') == answer
