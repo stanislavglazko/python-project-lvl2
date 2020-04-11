517ea2e4e82ac1f20cb5cd67178510be6f452517
stanislavglazko <stanislavglazko@gmail.com>
2020-04-11 11:21:25 +0300

    new gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,9 @@ from gendiff import generate_diff
 
 
 def main():
-    print(generate_diff(parse_args().first_file, parse_args().second_file,
-                        parse_args().format))
+    args = parse_args()
+    print(generate_diff(args.first_file, args.second_file,
+                        args.format))
 
 
 if __name__ == '__main__':
